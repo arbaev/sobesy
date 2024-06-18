047628b96f89e9041ea0df26be18a3c13009eba6
Tim Arbaev <timbrk@gmail.com>
2024-06-18 20:24:18 +0300

    feat: refactor mainMenu in my way

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,63 +1,100 @@
 <script setup lang="ts">
-import { ref, computed } from "vue"
+import { computed } from "vue"
 import { useUserStore } from "@/store/user"
 
 const userStore = useUserStore()
 
-const isAuth = computed<boolean>(() => {
-  return !!userStore.userId
+const itemsRightMenu = computed<IMenuItem[]>(() => {
+  return itemsMainMenu.value.filter((x) => x.align === "right")
 })
 
+enum MenuItemIDs {
+  add,
+  list,
+  stats,
+  login,
+  logout,
+}
+
 interface IMenuItem {
+  id: MenuItemIDs
   label: string
   icon: string
   path: string
   showAuth: boolean
+  align?: "left" | "right"
 }
 
-const items = ref<IMenuItem[]>([
-  {
-    label: "Авторизация",
-    icon: "pi pi-user",
-    path: "/auth",
-    showAuth: !isAuth.value,
-  },
-  {
-    label: "Добавить",
-    icon: "pi pi-plus",
-    path: "/",
-    showAuth: isAuth.value,
-  },
-  {
-    label: "Список собеседований",
-    icon: "pi pi-list",
-    path: "/list",
-    showAuth: isAuth.value,
-  },
-  {
-    label: "Статистика",
-    icon: "pi pi-chart-pie",
-    path: "/stats",
-    showAuth: isAuth.value,
-  },
-])
+const itemsMainMenu = computed<IMenuItem[]>(() => {
+  return [
+    {
+      id: MenuItemIDs.add,
+      label: "Добавить",
+      icon: "pi pi-plus",
+      path: "/",
+      showAuth: userStore.isAuth,
+    },
+    {
+      id: MenuItemIDs.list,
+      label: "Список собеседований",
+      icon: "pi pi-list",
+      path: "/list",
+      showAuth: userStore.isAuth,
+    },
+    {
+      id: MenuItemIDs.stats,
+      label: "Статистика",
+      icon: "pi pi-chart-pie",
+      path: "/stats",
+      showAuth: userStore.isAuth,
+    },
+    {
+      id: MenuItemIDs.login,
+      label: "Авторизация",
+      icon: "pi pi-user",
+      path: "/auth",
+      showAuth: !userStore.isAuth,
+      align: "right",
+    },
+    {
+      id: MenuItemIDs.logout,
+      label: "Выход",
+      icon: "pi pi-sign-out",
+      path: "/",
+      showAuth: userStore.isAuth,
+      align: "right",
+    },
+  ]
+})
+
+const clickOnMenu = (item: IMenuItem): void => {
+  if (item.id === MenuItemIDs.logout) {
+    userStore.userId = ""
+  }
+}
 </script>
 
 <template>
-  <AppMenubar :model="items" class="menu">
+  <AppMenubar :model="itemsMainMenu" class="menu">
     <template #item="{ item, props }">
-      <template v-if="item.showAuth">
+      <template v-if="item.showAuth && item.align !== 'right'">
         <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
           <span :class="item.icon" class="p-menuitem-icon"></span>
           <span class="ml-2">{{ item.label }}</span>
         </router-link>
       </template>
     </template>
-    <template #end v-if="isAuth">
-      <span @click="userStore.userId = ''" class="flex align-items-center menu-exit">
-        <span class="p-menuitem-icon pi pi-sign-out"></span>
-        <span class="ml-2">Выход</span>
-      </span>
+    <template #end>
+      <div class="align-end">
+        <template v-for="item in itemsRightMenu" :key="item.id">
+          <template v-if="item.showAuth">
+            <router-link :to="item.path" class="flex align-items-center" @click="clickOnMenu(item)">
+              <span :class="item.icon" class="p-menuitem-icon"></span>
+              <span class="ml-2">{{ item.label }}</span>
+            </router-link>
+          </template>
+        </template>
+      </div>
     </template>
   </AppMenubar>
 </template>
@@ -68,5 +105,11 @@ const items = ref<IMenuItem[]>([
 }
 .menu-exit {
   cursor: pointer;
+}
+.align-end {
+  display: flex;
+  flex-direction: row;
+  gap: 1.5rem;
+  padding: 0.5rem 0;
 }
 </style>
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,8 +1,12 @@
 import { defineStore } from "pinia"
-import { ref } from "vue"
+import { ref, computed } from "vue"
 
 export const useUserStore = defineStore("user", () => {
-  const userId = ref<string>("")
+  const userId = ref<string>("RRR")
 
-  return { userId }
+  const isAuth = computed<boolean>(() => {
+    return userId.value !== ""
+  })
+
+  return { userId, isAuth }
 })
