79e763bf22b807fada97eade5a992462070506c4
Tim Arbaev <timbrk@gmail.com>
2024-07-02 14:59:49 +0300

    feat: query for interviews list

--- src/views/PageList.vue
+++ src/views/PageList.vue
@@ -1,7 +1,41 @@
-<template>
-  <div>list</div>
-</template>
+<script setup lang="ts">
+import { ref, onMounted } from "vue"
+import { getFirestore, collection, query, orderBy, getDocs, deleteDoc, doc, where } from "firebase/firestore"
+import { useUserStore } from "@/stores/user"
+import { type IInterview } from "@/interfaces"
+import { useToast } from "primevue/usetoast"
 
-<script setup lang="ts"></script>
+const toast = useToast()
+
+const userStore = useUserStore()
+const db = getFirestore()
+
+const interviews = ref<IInterview[]>([])
+const isLoading = ref<boolean>(true)
+
+const getInterviews = async <T extends IInterview>(): Promise<T[] | []> => {
+  try {
+    const q = query(collection(db, `users/${userStore.userId}/interviews`), orderBy("createdAt", "desc"))
+    const querySnapshot = await getDocs(q)
+
+    return querySnapshot.docs.map((doc) => doc.data() as T)
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      toast.add({ severity: "error", summary: "Error", detail: error.message, life: 5000 })
+    }
+    return []
+  }
+}
+
+onMounted(async () => {
+  const listInterviews: Array<IInterview> = await getInterviews()
+  interviews.value = [...listInterviews]
+  isLoading.value = false
+})
+</script>
+
+<template>
+  <div>{{ interviews }}</div>
+</template>
 
 <style scoped></style>
